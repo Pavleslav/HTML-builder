c2d5ff484e230be9676155672da6c1bb814c8966
Pavel Dzehtsiarenka <dragrais23@gmail.com>
2023-05-08 21:23:41 +0300

    feat: 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require("fs");
+const path = require("path");
 
+function copyDir() {
+  fs.readdir(path.join(__dirname, "files-copy"), { withFileTypes: true }, (error, data) => {
+    if (error) {
+      return error;
+    } else {
+      data.forEach((file) => {
+        fs.unlink(path.join(__dirname, "files-copy", file.name), (error) => {});
+      });
+    }
+  });
+
+  fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, (error) => {
+    if (error) {
+      return error;
+    }
+  });
+
+  fs.readdir(path.join(__dirname, "files"), { withFileTypes: true }, (error, data) => {
+    if (error) {
+      return error;
+    } else {
+      data.forEach((file) => {
+        fs.copyFile(
+          path.join(__dirname, "files", file.name),
+          path.join(__dirname, "files-copy", file.name),
+          (error) => {}
+        );
+      });
+    }
+  });
+}
+copyDir();
